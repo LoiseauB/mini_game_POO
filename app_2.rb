7f76ac7efe84189dc076ef88643bda55c9dd15cd
LoiseauB <bapt.loiseau@gmail.com>
2022-07-19 16:42:38 +0200

    v2.0

---
+++ app_2.rb
@@ -0,0 +1,101 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+puts "------------------------------------------"
+puts "|-◊-  Call-of-Duty Programming War    -◊-|"
+puts "|                -v2.0-                  |"
+puts "|________________________________________|"
+puts "|    Tache de rester en vie le bleu !    |"
+puts "------------------------------------------"
+puts" "
+puts" "
+puts "Quel es ton nom de soldat ?"
+print "-> "
+user_name = gets.chomp
+puts" "
+puts" "
+puts "Garde à vous recrue #{user_name} !"
+puts" "
+
+user = HumanPlayer.new(user_name)
+player1 = Player.new("Jose Jalapeño on a stick")
+player2 = Player.new("Peanuts")
+enemies = [player1, player2]
+
+puts "On va te mettre direct dans le bain recrue. Voici tes deux adversaires #{player1.player_name} et #{player2.player_name}"
+
+while user.life_points > 0 && (player1.life_points > 0 || player2.life_points > 0)
+  user.show_state
+  puts " "
+  puts " "
+  puts "Quelle action veux-tu effectuer ?"
+  puts " "
+  puts "  a - chercher une meilleure arme"
+  puts "  b - chercher à te soigner"
+  puts " "
+  puts "  Attaquer un soldat ennemi :"
+  print "   0 - "
+  player1.show_state
+  print "   1 - "
+  player2.show_state
+
+  print "Entre ton action ici -> "
+  action = gets.chomp
+
+  while action != "a" && action != "b" && action != "0" && action != "1"
+    puts ""
+    puts "Action incorrecte ! 100 pompes maintenant et recommance !!"
+    puts "Quelle action veux-tu effectuer ?"
+    puts " "
+    puts "  a - chercher une meilleure arme"
+    puts "  b - chercher à te soigner"
+    puts " "
+    puts "  Attaquer un soldat ennemi :"
+    print "   0 - "
+    player1.show_state
+    print "   1 - "
+    player2.show_state
+
+    print "Entre ton action ici -> "
+    action = gets.chomp
+    puts " "
+  end
+
+  if action == "a"
+    puts " "
+    user.search_weapon
+  elsif action == "b"
+    puts " "
+    user.search_health_pack
+  elsif action == "0"
+    puts " "
+    user.attacks(player1)
+  elsif action == "1"
+    puts " "
+    user.attacks(player2)
+  end
+
+  puts "  "
+  puts" "
+  puts "À COUVERT ! Les autres joueurs t'attaquent !"
+  enemies.each do |player|
+    if player.life_points <= 0
+      puts " "
+      player.show_state
+    else
+      puts " "
+      player.attacks(user)
+    end
+  end
+
+end
+
+puts "La parite est terminée !"
+if user.life_points <= 0
+  puts "T'appelles ça être soldat ? T'as rien dans le slip ! Tu ne mérite pas d'avoir des couilles/ovaires !!!"
+else
+  puts "Bravo champion ! Tu as su rester en vie beau combat !"
+end
--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,8 @@
 class Player
   attr_accessor :player_name, :life_points
 
-  def initialize(player_name)
-    @player_name = player_name
+  def initialize(pname)
+    @player_name = pname
     @life_points = 10
   end
 
@@ -28,6 +28,58 @@ class Player
 
   def compute_damage
     return rand(1..6)
+  end
+
+end
+
+class HumanPlayer < Player
+  attr_accessor :weapon_level
+
+  def initialize(pname)
+    super(pname)
+    @weapon_level = 1
+    @life_points = 100
+    
+  end
+
+  def show_state
+    puts "#{@player_name} a #{@life_points} points de vie et une arme de niveau #{@weapon_level}"
+  end
+
+  def compute_damage
+    return rand(1..6)*@weapon_level
+  end
+
+  def search_weapon
+    dice = rand(1..6)
+    puts "Tu as trouvé une arme de niveau #{dice}"
+    if @weapon_level < dice
+      @weapon_level = dice
+      puts "Ton arme est maintenant de niveau #{@weapon_level}"
+    else
+      puts "Cette camelote ne sert à rien ! Tu as déjà une arme de niveau #{@weapon_level}"
+    end
+  end
+
+  def search_health_pack
+    dice = rand(1..6)
+    if dice == 1
+      puts "Il n'y a rien ici... FACK !"
+    elsif dice >= 2 && dice <= 5
+      puts "Génial tu as pu te soigné ! +50pts de vie"
+      @life_points += 50
+      if @life_points > 100
+        @life_points = 100
+        puts "Santé Max"
+      end
+    elsif dice == 6
+      puts "Génial tu as pu te soigné ! +80pts de vie"
+      @life_points += 80
+      if @life_points > 100
+        @life_points = 100
+        puts "Santé Max"
+      end
+    end
   end
 
 end
